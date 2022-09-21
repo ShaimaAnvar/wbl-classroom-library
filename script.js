16dcd02c5f3e6e96911f981e45aab147d9a28b89
Shaima <shaima.anvar@gmail.com>
2022-09-21 11:38:43 -0400

    changed buttons shape and modal font, added table hover and functionality for close button

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 
 $(document).ready(function(){
-    $("#div1").hide();
-    $("a").mouseenter(function(){
-        $("a").css("color", "red");
-    });
-    $("a").mouseleave(function(){
-        $("a").css("color", "blue");
-    });
+    $("#myModal").hide();
+    $("button").hover(function(){
+        $(this).css("color", "red");
+        },
+        function(){
+        $(this).css("color", "white");
+        });
     $(".detail").click(function(){
         var id = $(this).attr('id');
-        //console.log(id);
         switch(id){
             case 'detail1':
                 $("#bookTitle").text("Curious george learns the Alphabets");
@@ -17,7 +16,6 @@ $(document).ready(function(){
                 $("#bookPublisher").text("HMH Books for Young Readers");
                 $("#bookAge").text("2-3 years");
                 $("#bookImage").attr("src", "images/book1.png");
-                $("#div1").show();
                 break;
             case 'detail2':
                 $("#bookTitle").text("Pete the cat I love my white shoes");
@@ -25,7 +23,6 @@ $(document).ready(function(){
                 $("#bookPublisher").text("HarperCollins");
                 $("#bookAge").text("3-4 years");
                 $("#bookImage").attr("src", "images/book2.png");
-                $("#div1").show();
                 break;
             case 'detail3':
                 $("#bookTitle").text("Tom and jerry meet little quack ");
@@ -33,7 +30,6 @@ $(document).ready(function(){
                 $("#bookPublisher").text("Golden books");
                 $("#bookAge").text("2-5 years");
                 $("#bookImage").attr("src", "images/book3.png");
-                $("#div1").show();
                 break;
             case 'detail4':
                 $("#bookTitle").text("The Berenstain bears go to the doctor ");
@@ -41,10 +37,13 @@ $(document).ready(function(){
                 $("#bookPublisher").text("Random House Books for Young Readers");
                 $("#bookAge").text("2-4 years");
                 $("#bookImage").attr("src", "images/book4.png");
-                $("#div1").show();
                 break;
             default:
                 break;       
         }
+        $("#myModal").show();
+        $("#closeButton").click(function(){
+            $("#myModal").hide();
+        });
     })
 })   
